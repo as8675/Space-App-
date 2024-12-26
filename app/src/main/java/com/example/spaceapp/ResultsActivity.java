05a502215cb241ed50d456fceef008cd6b089441
Ayush Setpal <as8675@g.rit.edu>
2024-12-25 20:38:01 -0500

    Added Share Results button

--- app/src/main/java/com/example/spaceapp/ResultsActivity.java
+++ app/src/main/java/com/example/spaceapp/ResultsActivity.java
@@ -20,6 +20,8 @@ public class ResultsActivity extends AppCompatActivity {
          accuracyText = findViewById(R.id.accuracy_text);
          Button mainMenuButton = findViewById(R.id.main_menu_button);
          Button retakeQuizButton = findViewById(R.id.retake_quiz_button);
+         Button shareResultsButton = findViewById(R.id.share_results_button);
+         String appName = getString(R.string.app_name);
 
 
         // Get data from intent
@@ -48,5 +50,19 @@ public class ResultsActivity extends AppCompatActivity {
             startActivity(intent);
             finish();
         });
+
+        shareResultsButton.setOnClickListener(v -> {
+            String shareText = "I scored " + score + " in the Space Quiz!\n" +
+                    "Correct Answers: " + correctAnswers + "\n" +
+                    "Incorrect Answers: " + incorrectAnswers + "\n" +
+                    "Accuracy: " + (correctAnswers * 100 / totalQuestions) + "%\n" +
+                    "Think you can beat my score? Download " + appName + " now!";
+
+            Intent shareIntent = new Intent(Intent.ACTION_SEND);
+            shareIntent.setType("text/plain");
+            shareIntent.putExtra(Intent.EXTRA_TEXT, shareText);
+            startActivity(Intent.createChooser(shareIntent, "Share your quiz results"));
+        });
+
     }
 }
